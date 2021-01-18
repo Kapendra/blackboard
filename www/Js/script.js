8cc52a6739fe49d02da39773893098b721e2dd1d
kapendra <kapendrak012@gmail.com>
2021-01-18 23:39:07 +0545

    complete

---
+++ www/Js/script.js
@@ -0,0 +1,11 @@
+var config = {
+    apiKey: "AIzaSyAWqMx6V6il_GiQJmY8d3bGfOAuFJN299M",
+    authDomain: "blackboard-8bec9.firebaseapp.com",
+    databaseURL: "https://blackboard-8bec9-default-rtdb.firebaseio.com",
+    projectId: "blackboard-8bec9",
+    storageBucket: "blackboard-8bec9.appspot.com",
+    messagingSenderId: "580372703072",
+    appId: "1:580372703072:web:d17557cf74c3428a5ca005",
+    measurementId: "G-7Q57BYD1P1"
+};
+firebase.initializeApp(config);
